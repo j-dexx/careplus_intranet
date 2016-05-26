8199ec2814b88c11d1556aca96a2a403cfe7f249
Paul <pauloptimised@users.noreply.github.com>
2016-05-26 13:17:57 +0100

    Update Mailchimp

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
-  has_many :email_items, :dependent => :destroy
+  has_many :email_items, dependent: :destroy
 
   accepts_nested_attributes_for :email_items
 
@@ -9,7 +9,7 @@ class Email < ActiveRecord::Base
 
   before_create :set_list_name
 
-  validates :list_id, :presence => true
+  validates :list_id, presence: true
 
   accepts_nested_attributes_for :email_items, reject_if: proc { |attributes| attributes['email_item_id'].blank? }
 
@@ -21,19 +21,17 @@ class Email < ActiveRecord::Base
     api = Mailchimp::API.new('01e4730feb63b653b84f5e90cc0f90d0-us3')
 
     # campaignCreate(string apikey, string type, array options, array content, array segment_opts, array type_opts)
-    campaign_id = api.campaignCreate(
-      'type' => 'regular',
-      'options' => {
+    campaign_id = api.campaigns.create(
+      'regular',
+      {
         'list_id' => list_id,
         'subject' => subject,
         'from_email' => 'careplus@eskimosoup.co.uk',
         'from_name' => 'Care Plus Group',
         'to_name' => 'Care Plus Group Newsletter Subscribers',
         'auto_footer' => true
-       },
-      'content' => {
-        'url' => email_url(self, :host => ActionMailer::Base.default_url_options[:host])
-      }
+      },
+      'url' => email_url(self, host: ActionMailer::Base.default_url_options[:host])
     )
 
     update_attribute(:campaign_id, campaign_id)
@@ -43,11 +41,11 @@ class Email < ActiveRecord::Base
     api = Mailchimp::API.new('01e4730feb63b653b84f5e90cc0f90d0-us3')
 
     # campaignUpdate(string apikey, string cid, string name, mixed value)
-    api.campaignUpdate('cid' => campaign_id, 'name' => 'subject', 'value' => subject)
-    api.campaignUpdate(
-      'cid' => campaign_id,
-      'name' => 'content',
-      'value' => { 'url' => email_url(self, :host => ActionMailer::Base.default_url_options[:host]) }
+    api.campaigns.update(campaign_id, 'subject', subject)
+    api.campaigns.update(
+      campaign_id,
+      'content',
+      'url' => email_url(self, host: ActionMailer::Base.default_url_options[:host])
     )
   end
 
@@ -55,7 +53,7 @@ class Email < ActiveRecord::Base
     api = Mailchimp::API.new('01e4730feb63b653b84f5e90cc0f90d0-us3')
 
     # campaignSendTest(string apikey, string cid, array test_emails, string send_type)
-    api.campaignSendTest('cid' => campaign_id, 'test_emails' => [SITE_SETTINGS['Email']], 'send_type' => 'html')
+    api.campaigns.send_test(campaign_id, [SITE_SETTINGS['Email']], 'html')
   end
 
   def send_now
@@ -68,18 +66,17 @@ class Email < ActiveRecord::Base
   end
 
   def set_list_name
-    self.list_name = Email.campaigns.rassoc("#{list_id}").first
+    self.list_name = Email.campaigns.rassoc(list_id.to_s).first
   end
 
   def self.campaigns
     api = Mailchimp::API.new('01e4730feb63b653b84f5e90cc0f90d0-us3')
 
-    list = api.lists
-    unless list.blank? or list["data"].blank?
-      campaigns = list["data"].map{|x| x.values_at("name", "id")}
-    else
-      campaigns = []
-    end
+    list = api.lists.list
+    campaigns = if list.blank? || list['data'].blank?
+                  []
+                else
+                  list['data'].map { |x| x.values_at('name', 'id') }
+                end
   end
-
 end
